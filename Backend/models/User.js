0a4b8c3f504eff7411e69d31088d82a24679f694
pedros.hernandes <pedro.s.hernandes845@gmail.com>
2025-05-03 14:33:20 -0300

    Model user e futuramente controller

---
+++ Backend/models/User.js
@@ -0,0 +1,62 @@
+import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+const userSchema = new Schema({
+    nome: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    telefone: {
+        type: String
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    tipo: {
+        type: String,
+        required: true,
+        enum: ['admin', 'estudante', 'recrutador']
+    },
+    status:{
+        type: Boolean,
+        required: true
+    },
+    curso: { //averiguar dps se entra pois 
+        type: String,
+        required: function() { //obrigatorio para estudante
+            return this.tipo === 'estudante';
+        }
+    },
+    instituicao_ensino: {
+        type: String,
+        required: true
+    },
+    github: {
+        type: String
+    },
+    linkedin: {
+        type: String
+    },
+    sobre: {
+        type: String
+    },
+    portifolio: {
+        type: String
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    }
+}, {timestamps: true});
+const User = mongoose.model('User', userSchema);
+export default User;
