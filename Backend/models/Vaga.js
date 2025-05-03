458e9a41bd15e5622076ed27fbd470595ae1567a
pedros.hernandes <pedro.s.hernandes845@gmail.com>
2025-05-03 18:07:39 -0300

    Criação das models com base nas tabelas

---
+++ Backend/models/Vaga.js
@@ -0,0 +1,44 @@
+import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+const vagaSchema = new Schema({
+    titulo: {
+        type: String,
+        required: true
+    },
+    descricao: {
+        type: String,
+        required: true
+    },
+    requisitos: {
+        type: [String],
+        required: true
+    },
+    modalidade: {
+        type: String,
+        required: true
+    },
+    localizacao: {
+        type: String,
+        required: true
+    },
+    empresa:{
+        type: Object,
+        required:true
+    },
+    email_contato:{
+        type:String,
+        required:true
+    },
+    link_candidatura:{
+        type:String,
+        required:true
+    },
+    createdAt:{
+        type:Date,
+        default:Date.now
+    },
+}, {timestamps:true});
+
+const Vaga = mongoose.model('Vaga', vagaSchema);
+export default Vaga;
