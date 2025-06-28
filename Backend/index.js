699b22bc0f0b95b89c16688c8e253b0b3841cdbb
pedros.hernandes <pedro.s.hernandes845@gmail.com>
2025-06-27 22:29:16 -0300

    ajuste vagas externas e loop de verificação

--- Backend/index.js
+++ Backend/index.js
@@ -1,5 +1,8 @@
 import express from "express";
 import cors from "cors";
+import cron from "node-cron";
+import dotenv from "dotenv";
+import axios from "axios";
 //importar os routes como no ex abaixo
 import routesArtigo from "./Routers/Artigo/routesArtigo.js";
 import routesEmpresa from "./Routers/Empresa/routesEmpresa.js";
@@ -8,7 +11,6 @@ import routesUser from "./Routers/User/routesUser.js";
 import routesVaga from "./Routers/Vaga/routesVaga.js";
 import cookieparser from "cookie-parser";
 import routesVagaExterna from "./Routers/VagaExterna/routesVagaExterna.js";
-import dotenv from "dotenv";
 
 const app = new express();
 dotenv.config();
@@ -18,7 +20,7 @@ app.use(cookieparser());
 app.use(express.json());
 //cors frontend
 app.use(cors({
-    credentials:true, origin:"http://localhost:3000"
+    credentials: true, origin: "http://localhost:3000"
 }))
 app.use(express.static('public'));
 app.use("/artigos", routesArtigo);
@@ -26,6 +28,19 @@ app.use("/empresas", routesEmpresa);
 //app.use("/sessao", routesSessao);
 app.use("/vagas", routesVaga);
 app.use("/vagasExternas", routesVagaExterna);
+const task = cron.schedule('* 10 * * *', async () => {
+    
+    try {
+        console.log("Iniciando importação de vagas externas...");
+        await axios.post('http://localhost:5000/vagasExternas/ImportarVagas');
+        console.log("Vagas externas importadas com sucesso!");
+    } catch (error) {
+        console.error("Erro ao encontrar novas vagas externas");
+    }
+}, {
+    timezone: "America/Sao_Paulo"
+});
+task.execute();
 app.use("/users", routesUser);
 //usar os routes como no ex abaixo
 //app.use("/users", routes);
